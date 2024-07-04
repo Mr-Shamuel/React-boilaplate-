c020a0c69b281ea4398e5b87643cefdf7aea7a23
shamuel.molla <shamuel.molla@businessaccelerate.com.bd>
2024-07-04 11:19:04 +0600

    updated fontAwesome and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+ 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getTodoData } from './Redux/Todos/TodosAction';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 function App() {
   const {todosData} = useSelector((state) => state.todosData);
   const dispatch = useDispatch();
@@ -18,7 +18,9 @@ function App() {
 
   return (
     <div className="container vh-100 d-flex justify-content-center align-items-center">
-      <h1>Welcome to React Boilaplate  <FontAwesomeIcon icon={faCoffee} /> </h1>
+      <h1>Welcome to React Boilaplate  
+        <FontAwesomeIcon icon={faEnvelope} />
+        </h1>
      
        
       
